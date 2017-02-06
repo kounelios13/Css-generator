7182d25c8891c34bf016783eb78a4ed9bb3a98bf
kounelios13 <pkounelios@gmail.com>
2017-02-06 13:33:08 +0200

    removed dead code

--- bower_components/DomStorage/domStorage.js
+++ bower_components/DomStorage/domStorage.js
@@ -1,12 +1,16 @@
 function DomStorage() {
 	var cache = {};
 	function getFromCache(selector){
+		if(!selector)
+			throw new TypeError("Please provide a selector");
 		if(!cache[selector]){
 			cache[selector] = $(selector);
 		}
 		return cache[selector];
 	}
 	function updateInCache(selector){
+		if(!selector)
+			throw new TypeError("Please provide a selector");
 		if(!cache[selector]){
 			throw new TypeError("The selector you want to update does not exist in cache");
 		}
